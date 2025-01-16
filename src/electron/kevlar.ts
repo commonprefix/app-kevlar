ecc85283c1365ab18e60f552b862b78a8e3b8eaa
Odysseas Sofikitis <odysseas@commonprefix.com>
2025-01-16 15:01:52 +0200

    Fix date format on log

--- src/electron/kevlar.ts
+++ src/electron/kevlar.ts
@@ -14,7 +14,7 @@ export default class KevlarHandler {
         this.port = 8546;
     }
 
-    async start(onStartCallback: () => void,onCloseCallback: () => void): Promise<void> {
+    async start(onStartCallback: () => void, onCloseCallback: () => void): Promise<void> {
         console.log('Starting Kevlar');
 
         const isPortInUse = (port: number): Promise<boolean> => {
@@ -41,22 +41,39 @@ export default class KevlarHandler {
         }
         onStartCallback();
 
+        const formatDate = (date: Date) => {
+            const day = String(date.getDate()).padStart(2, '0');
+            const month = String(date.getMonth() + 1).padStart(2, '0');
+            const year = date.getFullYear();
+            const hours = String(date.getHours()).padStart(2, '0');
+            const minutes = String(date.getMinutes()).padStart(2, '0');
+            const seconds = String(date.getSeconds()).padStart(2, '0');
+            return `${day}/${month}/${year}, ${hours}:${minutes}:${seconds}`;
+        };
+
         this.kevlar = fork(getKevlarPath(), { silent: true });
 
         this.kevlar.stdout?.on('data', (data: any) => {
             const logMessage = data.toString();
-            ipcWebContentsSend('logs', this.logWindow.webContents, { message: logMessage, level: 'info', timestamp: new Date().toISOString() });
+
+            ipcWebContentsSend('logs', this.logWindow.webContents,
+                { message: logMessage, level: 'info', timestamp: formatDate(new Date()) });
         });
 
         this.kevlar.stderr?.on('data', (data: any) => {
             const logMessage = data.toString();
-            ipcWebContentsSend('logs', this.logWindow.webContents, { message: logMessage, level: 'error', timestamp: new Date().toISOString() });
+            ipcWebContentsSend('logs', this.logWindow.webContents,
+                { message: logMessage, level: 'error', timestamp: formatDate(new Date()) });
             console.error(logMessage);
         });
 
         this.kevlar.on('close', (code: number) => {
             console.log(`Kevlar process exited with code ${code}`);
-            ipcWebContentsSend('logs', this.logWindow.webContents, { message: `Kevlar process exited with code ${code}`, level: 'info', timestamp: new Date().toISOString() });
+            ipcWebContentsSend('logs', this.logWindow.webContents,
+                {
+                    message: `Kevlar process exited with code ${code}`,
+                    level: 'info', timestamp: formatDate(new Date())
+                });
             this.kevlar = null;
             onCloseCallback();
         });
